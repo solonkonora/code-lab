7217788f60d6acc8982dac85e1518b9b7ecde5ba
Nkwada Nora Solonko <nkwadanora@mail.com>
2024-01-13 13:18:13 -0800

    restructuring files

--- assets/scripts/page3.js
+++ assets/scripts/page3.js
@@ -3,14 +3,13 @@ function showFinalScore() {
     finalScoreContainer.innerText = `Your Final Score: ${correctAnswers} out of ${questions.length}`;
   }
   
-  function resetQuiz() {
-    currentQuestionIndex = 0;
-    questions = [];
-    correctAnswers = 0;
-    document.getElementById('question').innerText = '';
-    document.getElementById('options-container').innerHTML = '';
-    document.getElementById('final-score').innerText = '';
-  }
+//   function resetQuiz() {
+//     currentQuestionIndex = 0;
+//     questions = [];
+//     correctAnswers = 0;
+//     document.getElementById('question').innerText = '';
+//     document.getElementById('options-container').innerHTML = '';
+//     document.getElementById('final-score').innerText = '';
+//   }
   
   showFinalScore();
-  
